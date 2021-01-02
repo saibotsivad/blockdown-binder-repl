02b58a764a6ce58b31d98a9bd7784759bded5f3c
Tobias Davis <accounts@davistobias.com>
2021-01-02 00:05:49 -0600

    init: making a file tree explorer thingy

---
+++ src/File.svelte
@@ -0,0 +1,25 @@
+<script>
+	import Icon from 'svelte-awesome'
+	import { file } from 'svelte-awesome/icons'
+
+	export let name = ''
+	export let icons
+
+	$: extension = name && name.split('.').pop()
+</script>
+
+<style>
+	button:hover {
+		background-color: lightgray;
+	}
+	div {
+		padding-left: 1rem;
+	}
+</style>
+
+<div>
+	<button>
+		<Icon data={ icons && icons(extension) || file } />
+		{ name }
+	</button>
+</div>
